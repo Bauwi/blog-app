60efcdafeda9a20a7f9b1a241dbba83abf2dd13a
Beauwie <kvn.philippe@gmail.com>
2017-11-22 09:23:12 +0100

    deploy test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,22 @@ import * as firebase from 'firebase';
 import 'firebase/firestore';
 import moment from 'moment';
 
+// const config = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// };
+
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: 'AIzaSyAnR5WQOqx2Pn-wwLZJLuqhmUpWn7DwZZ8',
+  authDomain: 'blog-app-1de4a.firebaseapp.com',
+  databaseURL: 'https://blog-app-1de4a.firebaseio.com',
+  projectId: 'blog-app-1de4a',
+  storageBucket: 'blog-app-1de4a.appspot.com',
+  messagingSenderId: '280995665949'
 };
 
 firebase.initializeApp(config);
